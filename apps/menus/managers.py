30caddbcf169813bdf23d166cc063d679bad500d
Rense VanderHoek <rense@me.com>
2016-09-12 16:07:26 +0200

    menu-item-manager indentation fix

--- apps/menus/managers.py
+++ apps/menus/managers.py
@@ -3,6 +3,5 @@ from mptt.managers import TreeManager
 
 class MenuItemManager(TreeManager):
 
-        def get_main_menu(self):
-            return self.model.published.all().filter(parent__isnull=True)
-
+    def get_main_menu(self):
+        return self.model.published.all().filter(parent__isnull=True)
